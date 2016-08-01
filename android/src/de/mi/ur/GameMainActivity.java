6f98dc844380faa9eaddf3df4ffdbf5eb0420030
blueCat11 <blue.cat@t-online.de>
2016-08-01 22:34:22 +0200

    GameMainActivity added Activities in Manifest eingetragen ;) StartActivity Intent-Logik soweit möglich implementiert

---
+++ android/src/de/mi/ur/GameMainActivity.java
@@ -0,0 +1,16 @@
+package de.mi.ur;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Anna-Marie on 01.08.2016.
+ */
+public class GameMainActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_main_activity);
+
+
+    }
+}
--- android/src/de/mi/ur/StartActivity.java
+++ android/src/de/mi/ur/StartActivity.java
@@ -2,6 +2,7 @@ package de.mi.ur;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,7 +11,7 @@ import android.widget.EditText;
 /**
  * Created by maxiwindl on 01.08.16.
  */
-public class StartActivity extends Activity {
+public class StartActivity extends Activity implements View.OnClickListener {
 
     private Button buttonTutorial;
     private Button buttonPractice;
@@ -26,22 +27,37 @@ public class StartActivity extends Activity {
 
     }
 
+    //geändert...
     private void setupUI() {
         buttonTutorial = (Button) findViewById(R.id.start_tutorial_button);
-        setOnClickListener (buttonTutorial);
         buttonPractice = (Button) findViewById(R.id.start_practice_button);
-        setOnClickListener(buttonPractice);
         buttonGame = (Button) findViewById(R.id.start_game_button);
-        setOnClickListener(buttonGame);
         buttonProgress = (Button) findViewById(R.id.start_progress_button);
-        setOnClickListener(buttonProgress);
+
     }
 
-    private void setOnClickListener (Button button) {
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick (View v) {
 
-            }
-        });
+    //nicht vollständig implementiert (Practice und Progress fehlen
+    @Override
+    public void onClick(View v) {
+        Intent i = null;
+        switch (v.getId()){
+            case R.id.start_tutorial_button:
+                i = new Intent(StartActivity.this, TutorialMainActivity.class);
+                break;
+            case R.id.start_practice_button:
+                break;
+            case R.id.start_game_button:
+                i= new Intent (StartActivity.this, GameMainActivity.class);
+                break;
+            case R.id.start_progress_button:
+                break;
+            default:
+                break;
+        }
+        if(i!=null){
+            startActivity(i);
+        }
+
     }
 }
