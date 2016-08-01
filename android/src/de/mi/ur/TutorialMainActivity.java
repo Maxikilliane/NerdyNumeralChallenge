a226719c233658f454014e8be4e6df35aa58170c
blueCat11 <blue.cat@t-online.de>
2016-08-01 21:52:24 +0200

    ExplanationActivity und TutorialMainActivity angelegt

---
+++ android/src/de/mi/ur/TutorialMainActivity.java
@@ -0,0 +1,18 @@
+package de.mi.ur;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Anna-Marie on 01.08.2016.
+ */
+public class TutorialMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tutorial_main_activity);
+
+
+    }
+}
