c59718293536b24f55eea29307b7282d70fedabb
Maxikilliane <maxi@webforuse.de>
2016-08-01 21:25:04 +0200

    StarActivity

--- core/src/de/mi/ur/sprites/Pit.java
+++ core/src/de/mi/ur/sprites/Pit.java
@@ -13,9 +13,18 @@ public class Pit {
     private Random random;
     private Vector3 position;
     private Texture pit;
+    private Vector3 velocity;
+    private static final int MOVEMENT = 100;
+
 
     public Pit () {
         pit = new Texture("pit.png");
+        position = new Vector3(0, 0, 0);
+        velocity = new Vector3(0, 0, 0);
+    }
+
+    public Texture getTexture () {
+        return pit;
     }
 
     private void createNewPit() {
@@ -23,6 +32,26 @@ public class Pit {
         //int pitWidth = random.nextInt()
 
     }
+
+    public int getWidth () {
+        return pit.getWidth();
+    }
+
+    public void update(float dt) {
+
+        //mulitploziert alles mit delta-time
+        velocity.scl(dt);
+        position.add(MOVEMENT * dt, velocity.y, 0);
+        if (position.y <= pit.getHeight()) {
+            position.y = pit.getHeight();
+        }
+        velocity.scl(1 / dt);
+
+
+
+    }
+
+
 
 
 
