2f7749fed6a436cfdcbf419b138e9e0925324925
RiaRentnueg <lydia.guentner@web.de>
2016-08-02 22:48:12 +0200

    test comment removed

--- android/src/de/mi/ur/StartActivity.java
+++ android/src/de/mi/ur/StartActivity.java
@@ -16,9 +16,7 @@ public class StartActivity extends Activity {
     private Button buttonPractice;
     private Button buttonGame;
     private Button buttonProgress;
-
-    //test comment
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
