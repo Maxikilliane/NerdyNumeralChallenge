05f4015fd79d6956ac0d4363f35edf5040249e17
Maxikilliane <maxi@webforuse.de>
2016-08-04 16:34:19 +0200

    Vorschlag für Button layout

--- android/src/de/mi/ur/StartActivity.java
+++ android/src/de/mi/ur/StartActivity.java
@@ -2,6 +2,7 @@ package de.mi.ur;
 
 import android.app.Activity;
 import android.app.Application;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -39,11 +40,27 @@ public class StartActivity extends Activity {
         setOnClickListener(buttonProgress);
     }
 
-    private void setOnClickListener (Button button) {
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick (View v) {
+    public void setOnClickListener(View v) {
+        /*Intent i = null;
+        switch (v.getId()){
+            case R.id.start_tutorial_button:
+                i = new Intent(StartActivity.this, TutorialMainActivity.class);
+                break;
+            case R.id.start_practice_button:
+                   // i = new Intent (StartActivity.this, :
+                break;
+            case R.id.start_game_button:
+                i= new Intent (StartActivity.this, GameMainActivity.class);
+                break;
+            case R.id.start_progress_button:
+                //i = new Intent (StartActivity.this, )
+                break;
+            default:
+                break;
+        }
+        if(i!=null){
+            startActivity(i);
+        }*/
 
-            }
-        });
     }
 }
