2b8c2520efae1dd093390cdd6974af49308ff966
blueCat11 <blue.cat@t-online.de>
2016-08-05 15:19:32 +0200

    QuestionLogic continued - Alle Fragetypen weitgehend funktionsfähig -spezifizierbar durch Angabe des Start und Zielsystems und der maximalen Anzahl Stellen des Startsystems
    evtl toDo
     -maximale (Dezimale) Größe für Zielsystem angeben oder wenigstens maximale Anzahl Stellen für Zielsystem angeben
     -TutorialQuestion eleganter machen!!!!

--- android/src/de/mi/ur/QuestionLogic/FreeTextQuestion.java
+++ android/src/de/mi/ur/QuestionLogic/FreeTextQuestion.java
@@ -4,8 +4,26 @@ package de.mi.ur.QuestionLogic;
  * Created by Anna-Marie on 02.08.2016.
  */
 public class FreeTextQuestion extends Question {
+    private String question;
+    private String rightAnswer;
 
     public FreeTextQuestion(int numeral1Base, int numeral2Base, int maxDigits) {
         super(numeral1Base, numeral2Base, maxDigits);
+
+        this.question = generateNumWithMaxDigits(numeral1Base, maxDigits);
+        this.rightAnswer = convertFromNumeralToNumeral(question, numeral1Base, numeral2Base);
+
+    }
+
+    public String getQuestionString(){
+        return question;
+    }
+
+    public String getRightAnswerString(){
+        return rightAnswer;
+    }
+
+    public boolean isCorrectAnswer(String answer){
+        return answer.equals(rightAnswer);
     }
 }
