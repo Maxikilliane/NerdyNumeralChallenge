28060537992fc629652e18abe5f8ab9f26cb7ef9
RiaRentnueg <lydia.guentner@web.de>
2016-09-07 11:18:59 +0200

    Toolbar zur Game_Help_Activity hinzugefügt

--- android/src/de/mi/ur/Activities/GameHelpActivity.java
+++ android/src/de/mi/ur/Activities/GameHelpActivity.java
@@ -2,15 +2,34 @@ package de.mi.ur.Activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import de.mi.ur.R;
 
 /**
  * Created by Anna-Marie on 15.08.2016.
  */
-public class GameHelpActivity extends Activity {
+public class GameHelpActivity extends AppCompatActivity {
+    private Toolbar myToolbar;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game_help_activity);
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        myToolbar = (Toolbar) findViewById(R.id.game_help_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setTitle(R.string.game_help_toolbar_headline);
+        myToolbar.setNavigationIcon(R.drawable.toolbar_back);
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
