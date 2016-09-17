34a8adf8ddd63a485d1173b3723e7fcf5f21d82e
blueCat11 <blue.cat@t-online.de>
2016-09-17 11:11:13 +0200

    Bugfix
    Freetext (richtige Antwort + Keyboardfokus wiederhergestellt)

--- android/src/de/mi/ur/Activities/PracticeActivity.java
+++ android/src/de/mi/ur/Activities/PracticeActivity.java
@@ -298,7 +298,7 @@ public class PracticeActivity extends AppCompatActivity implements FreeTextQuest
                 break;
             default:
                 currentQuestion = new FreeTextQuestion(numeral1Base, numeral2Base, questionLength);
-                
+
         }
     }
 
